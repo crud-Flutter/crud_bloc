44e6816c14a43fb2d5b2b5d12e4f2aecdcf78138
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-22 16:28:45 -0300

    Usage code_builder

--- lib/src/bloc_generator.dart
+++ lib/src/bloc_generator.dart
@@ -2,128 +2,146 @@ import 'dart:async';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
+import 'package:code_builder/code_builder.dart';
 import 'package:crud_generator/crud_generator.dart';
-import 'package:flutter_persistence_api/flutter_persistence_api.dart';
+import 'package:flutter_persistence_api/flutter_persistence_api.dart'
+    as annotation;
 import 'package:source_gen/source_gen.dart';
 
-class BlocGenerator extends GeneratorForAnnotation<Entity> {
+class BlocGenerator
+    extends GenerateEntityClassForAnnotation<annotation.Entity> {
+  TypeChecker fieldAnnotation = TypeChecker.fromRuntime(annotation.Field);
+
   @override
   FutureOr<String> generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
-    var generateClass = GenerateBlocClass(element.name);
-    var fieldAnnotation = TypeChecker.fromRuntime(Field);
-    for (var field in (element as ClassElement).fields) {
-      generateClass.addField(field.type.name, field.name,
-          persistField: fieldAnnotation.hasAnnotationOfExact(field));
-    }
-    return generateClass.build();
+    name = '${element.name}Bloc';
+    this.element = element;
+    extend = refer('BlocBase');
+    _declareField();
+    _constructor();
+    _methodSetEntity();
+    _methodInserOrUpdate();
+    _methodDispose();
+    _methodDelete();
+    // var generateClass = GenerateBlocClass(element.name);
+    // var fieldAnnotation = TypeChecker.fromRuntime(Field);
+    // for (var field in (element as ClassElement).fields) {
+    //   generateClass.declareField(field.type.name, field.name,
+    //       persistField: fieldAnnotation.hasAnnotationOfExact(field));
+    // }
+    // return generateClass.build();
+    return "import 'package:bloc_pattern/bloc_pattern.dart';"
+            "import 'package:rxdart/rxdart.dart';"
+            "import '${element.name.toLowerCase()}.repository.dart';" +
+        build();
   }
-}
 
-class GenerateBlocClass extends GenerateEntityClassAbstract {
-  Map<String, String> fieldsIgnored = {};
-
-  GenerateBlocClass(String name)
-      : super(name, classSuffix: 'Bloc', parentClass: 'BlocBase') {
-    generateClass.writeln('String _documentId;');
-  }
-
-  @override
-  void addImports() {
-    importEntity();
-    importGenerate('repository');
-    generateClass.writeln('import \'package:bloc_pattern/bloc_pattern.dart\';');
-    generateClass.writeln('import \'package:rxdart/rxdart.dart\';');
+  void _declareField() {
+    declareField(refer('String'), '_documentId');
+    declareField(refer('var'), '_repository',
+        assignment: Code('${element.name}Repository()'));
+    declareMethod('list',
+        returns: refer('Stream<List<$entityClass>>'),
+        lambda: true,
+        type: MethodType.getter,
+        body: Code('_repository.list()'));
+    elementAsClass.fields.forEach((field) {
+      declareField(refer(field.type.name), '_${field.name}');
+      declareField(refer('var'), '_${field.name}Controller',
+          assignment: Code('BehaviorSubject<${field.type.name}>()'));
+      declareMethod('out${field.name}',
+          returns: refer('Stream<${field.type.name}>'),
+          type: MethodType.getter,
+          lambda: true,
+          body: Code('_${field.name}Controller.stream'));
+      declareMethod(field.name,
+          type: MethodType.setter,
+          requiredParameters: [
+            Parameter((b) => b
+              ..name = 'value'
+              ..type = refer(field.type.name))
+          ],
+          lambda: true,
+          body: Code('_${field.name}Controller.sink.add(value)'));
+    });
   }
 
   void _constructor() {
-    generateClass.writeln('$name() {');
-    fields.forEach((field, type) {
-      generateClass
-          .writeln('_$field' 'Controller.listen((value) => _$field = value);');
+    var constructorVar = BlockBuilder();
+    elementAsClass.fields.forEach((field) {
+      constructorVar.statements.add(Code(
+          '_${field.name}Controller.listen((value) => _${field.name} = value);'));
     });
-    generateClass.writeln('}');
-  }
-
-  @override
-  void generateFieldDeclaration(type, name, {bool persistField = false}) {
-    generateClass.writeln('$type ' + (persistField ? '_' : '') + '$name;');
-    if (!persistField) {
-      fieldsIgnored[name] = type;
+    if (constructorVar.statements.length > 0) {
+      declareConstructor(body: constructorVar.build());
     }
   }
 
-  void _gettters() {
-    fields.forEach((field, type) {
-      generateClass
-          .writeln('var _$field' 'Controller = BehaviorSubject<$type>();');
-      generateClass
-          .writeln('Stream<$type> get $field => _$field' 'Controller.stream;');
+  void _methodSetEntity() {
+    var entityCode = BlockBuilder();
+    elementAsClass.fields.forEach((field) {
+      entityCode.statements
+          .add(Code('${field.name} = ${entityInstance}.${field.name};'));
     });
+    if (entityCode.statements.isNotEmpty) {
+      entityCode.statements
+          .insert(0, Code('_documentId = $entityInstance.documentId();'));
+      declareMethod('set$entityClass',
+          requiredParameters: [
+            Parameter((b) => b
+              ..name = entityInstance
+              ..type = refer(entityClass))
+          ],
+          body: entityCode.build());
+    }
   }
 
-  void _setters() {
-    fields.forEach((field, type) {
-      generateClass.writeln('void set$field($type value) => _$field'
-          'Controller.sink.add(value);');
+  void _methodInserOrUpdate() {
+    var insertOrUpdateCode = BlockBuilder();
+    elementAsClass.fields.forEach((field) {
+      if (fieldAnnotation.hasAnnotationOfExact(field)) {
+        insertOrUpdateCode.statements
+            .add(Code('..${field.name} = _${field.name}'));
+      }
     });
+    if (insertOrUpdateCode.statements.isNotEmpty) {
+      insertOrUpdateCode.statements
+          .insert(0, Code('var $entityInstance = $entityClass()'));
+      insertOrUpdateCode.statements.add(Code(';'));
+      insertOrUpdateCode.statements
+          .add(Code('if (_documentId?.isEmpty ?? true) {'));
+      insertOrUpdateCode.statements
+          .add(Code('return _repository.add($entityInstance);'));
+      insertOrUpdateCode.statements.add(Code('} else {'));
+      insertOrUpdateCode.statements.add(
+          Code('return _repository.update(_documentId, $entityInstance);'));
+      insertOrUpdateCode.statements.add(Code('}'));
+    }
+    declareMethod('insertOrUpdate',
+        returns: refer('Future<dynamic>'), body: insertOrUpdateCode.build());
   }
 
-  void _setEntity() {
-    generateClass.writeln('void set$entityClass($entityClassInstance) {');
-    generateClass.writeln('_documentId = $entityInstance.documentId();');
-    fields.forEach((field, type) {
-      generateClass.writeln('set$field($entityInstance.$field);');
+  void _methodDelete() {
+    declareMethod('delete',
+        returns: refer('Future<void>'),
+        requiredParameters: [
+          Parameter((b) => b
+            ..name = 'documentId'
+            ..type = refer('String'))
+        ],
+        lambda: true,
+        body: Code('_repository.delete(documentId)'));
+  }
+
+  void _methodDispose() {
+    var disposeCode = BlockBuilder();
+    elementAsClass.fields.forEach((field) {
+      disposeCode.statements.add(Code('_${field.name}Controller.close();'));
     });
-    fieldsIgnored.forEach((field, type){
-      generateClass.writeln('$field = $entityInstance.$field;');
-    });
-    generateClass.writeln('}');
-  }
-
-  void _insertOrUpdate() {
-    generateClass.writeln('void insertOrUpdate() {');
-    generateClass.writeln('var $entityInstance = $entityClass()');
-    fields.forEach((field, type) {
-      generateClass.writeln('..$field = _$field');
-    });
-    generateClass.writeln(';');
-    generateClass.writeln('if (_documentId?.isEmpty ?? true) {');
-    generateClass.writeln('_repository.add($entityInstance);');
-    generateClass.writeln('} else {');
-    generateClass.writeln('_repository.update(_documentId, $entityInstance);');
-    generateClass.writeln('}');
-    generateClass.writeln('}');
-  }
-
-  void _repository() {
-    generateClass.writeln('var _repository = $classPrefix' 'Repository();');
-  }
-
-  void _dispose() {
-    generateClass.writeln('void dispose() {');
-    fields.forEach((field, type) {
-      generateClass.writeln('_$field' 'Controller.close();');
-    });
-    generateClass.writeln('super.dispose();');
-    generateClass.writeln('}');
-  }
-
-  void _list() {
-    generateClass
-        .writeln('get $nameLowerCase' 's => _repository.$nameLowerCase' 's;');
-  }
-
-  @override
-  String build() {
-    _constructor();
-    _repository();
-    _setEntity();
-    _setters();
-    _gettters();
-    _list();
-    _insertOrUpdate();
-    _dispose();
-    return super.build();
+    if (disposeCode.statements.isNotEmpty) {
+      disposeCode.statements.add(Code('super.dispose();'));
+      declareMethod('dispose', body: disposeCode.build());
+    }
   }
 }
